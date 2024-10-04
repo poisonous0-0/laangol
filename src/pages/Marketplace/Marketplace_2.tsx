d2926c5ef8fa28cc0ffe3abb8ec6bee8dbd55258
Maaz Bin Hossain <mhossainsaki@gmail.com>
2024-10-04 23:43:26 +0600

    marketplace routing

--- src/pages/Marketplace/Marketplace_2.tsx
+++ src/pages/Marketplace/Marketplace_2.tsx
@@ -1,15 +1,18 @@
 import Marketplace_top from "../../components/Marketplace_top/Marketplace_top";
 import ProductCard from "../../components/Dynamic_card/Product_card";
 import fertilizers from "../../assets/fertilizers.png";
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 const Marketplace_2 = () => {
+	const { category } = useParams(); // Get the category from URL
+
 	return (
 		<>
 			<Marketplace_top />
 			<div className="marketplace_items flex flex-col items-center justify-center">
 				<div className="items_banner mt-5 w-max p-2 bg-lime-100 text-2xl text-lime-200 rounded-md">
-					<h1>Vegetables</h1>
+					<h1>{category}</h1> {/* Dynamically display the category */}
 				</div>
 
 				<div className="mt-8 items grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 items-center justify-between">
