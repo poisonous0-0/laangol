8e8d9e9669f688a29680f333318d3c0facb46c80
Md. Kamrul Hassan <98039269+Kamrul-hassan1438@users.noreply.github.com>
2024-10-06 19:27:56 +0600

    updat3 10

--- src/components/Dynamic_card/Product_card.tsx
+++ src/components/Dynamic_card/Product_card.tsx
@@ -3,6 +3,7 @@ import Dynamic_button from "../Button/Dynamic_button";
 import defaultSellerImage from "../../assets/user.png"; // Path to the default seller image
 
 interface ProductCardProps {
+	product_id: number;
 	imageSrc: string;
 	productName: string;
 	sellerName: string;
@@ -23,12 +24,18 @@ const Product_card: React.FC<ProductCardProps> = ({
 				<img
 					src={imageSrc}
 					alt={productName}
-					className="w-60 h-w-60 min-w-60 min-h-60 object-cover rounded-full border border-lime-200"
+					className="w-60 h-60 min-w-60 min-h-60 object-cover rounded-full border border-lime-200"
 				/>
 			</div>
 			<div className="product_card_content flex flex-col items-center text-lime-700">
 				<h1>{productName}</h1>
 				<p>Seller: {sellerName}</p>
+				{/* Display the seller's image if available, otherwise use the default image */}
+				<img
+					src={sellerImage || defaultSellerImage}
+					alt={sellerName}
+					className="w-12 h-12 object-cover rounded-full border border-gray-300 mt-2"
+				/>
 				<Dynamic_button
 					initialLabel={price.toString() + "/KG BDT "}
 					hoverLabel="Add to cart"
